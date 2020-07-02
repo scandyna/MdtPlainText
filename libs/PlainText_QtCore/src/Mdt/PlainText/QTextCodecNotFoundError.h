bd1989ea9f1cddfb73e153081ef0628cf5164c16
Philippe Steinmann <philippe.steinmann@gmail.com>
2020-07-02 04:49:53 +0200

    Add exceptions

---
+++ libs/PlainText_QtCore/src/Mdt/PlainText/QTextCodecNotFoundError.h
@@ -0,0 +1,46 @@
+/****************************************************************************
+ **
+ ** MdtPlainText - A C++ library to read and write simple plain text
+ ** using the boost Spirit library.
+ **
+ ** Copyright (C) 2020-2020 Philippe Steinmann.
+ **
+ ** This program is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU Lesser General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ **
+ ****************************************************************************/
+#ifndef MDT_PLAIN_TEXT_QTEXT_CODEC_NOT_FOUND_ERROR_H
+#define MDT_PLAIN_TEXT_QTEXT_CODEC_NOT_FOUND_ERROR_H
+
+#include "QRuntimeError.h"
+#include "mdt_plaintext_qtcore_export.h"
+
+namespace Mdt{ namespace PlainText{
+
+  /*! \brief Error thrown when a QTextCodec could not be found
+   */
+  class MDT_PLAINTEXT_QTCORE_EXPORT QTextCodecNotFoundError : public QRuntimeError
+  {
+   public:
+
+    /*! \brief Construct a error
+     */
+    explicit QTextCodecNotFoundError(const QString & what)
+     : QRuntimeError(what)
+    {
+    }
+  };
+
+}} // namespace Mdt{ namespace PlainText{
+
+#endif // #ifndef MDT_PLAIN_TEXT_QTEXT_CODEC_NOT_FOUND_ERROR_H
