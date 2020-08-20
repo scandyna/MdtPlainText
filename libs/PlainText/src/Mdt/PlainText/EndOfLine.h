5745108e18e6b3a35af7a2e5021dd4122f425b94
Philippe Steinmann <philippe.steinmann@gmail.com>
2020-08-20 05:30:16 +0200

    Implement nativeEndOfLine()

--- libs/PlainText/src/Mdt/PlainText/EndOfLine.h
+++ libs/PlainText/src/Mdt/PlainText/EndOfLine.h
@@ -17,9 +17,23 @@ namespace Mdt{ namespace PlainText{
                    A file writer will open in text mode
                    which will append the platform native end-of-line terminators */
     Lf,     /*!< LF, \\n in C/C++, used on Unix (Linux, mac OS-X, etc) */
-    CrLf,   /*!< CR LF, \\r\\n in C/C++, used on mainly on MS-DOS and Microsoft Windows */
+    CrLf,   /*!< CR LF, \\r\\n in C/C++, used mainly on MS-DOS and Microsoft Windows */
     Cr      /*!< CR, \\r in C/C++, used mainly on mac OS prior to OS-X */
   };
+
+  /*! \brief Get the native end of line
+   */
+  inline
+  constexpr EndOfLine nativeEndOfLine() noexcept
+  {
+#if defined MDT_PLAIN_TEXT_OS_UNIX
+    return EndOfLine::Lf;
+#elif defined MDT_PLAIN_TEXT_OS_WINDOWS
+    return EndOfLine::CrLf;
+#else
+    return EndOfLine::Cr;
+#endif
+  }
 
 }} // namespace Mdt{ namespace PlainText{
 
